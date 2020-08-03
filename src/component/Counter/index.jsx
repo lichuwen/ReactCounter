ef9918583fb188cdefdc6690205f8b6762c4f15e
lichuwen <545759585@qq.com>
2020-08-04 07:29:07 +0800

    [redux][karen] refactor:split decrease&increase component

--- src/component/Counter/index.jsx
+++ src/component/Counter/index.jsx
@@ -1,58 +1,49 @@
+  
 import React from 'react';
-class Counter extends React.Component{
 
-    constructor(props){
-        super(props)
+class Counter extends React.Component {
+
+    constructor(props) {
+        super(props);
         this.state = {
-            number: 0,
-            count: this.props.count
-        };
-        this.reduce = this.reduce.bind(this);
-        this.increase = this.increase.bind(this);
+            number: 0
+        }
     }
 
+    increase = () => {
+        this.setState({
+            number: this.state.number + 1
+        });
+        this.props.increase()
+    };
+
+    decrease = () => {
+        this.setState((preState) => ({
+            number: preState.number - 1
+            })
+        )  
+        this.props.decrease()
+    };
+
     static getDerivedStateFromProps(Props,State){
-        if(State.count != Props.count){
+        if(State.count !== Props.count){
             return{
                 number:0
             }
         }
     }
 
-    increase = () => {
-        this.setState((preState) => ({
-            number: preState.number + 1
-            })
-        )
-        this.props.countTotal(1)
-    }
-
-    // componentWillReceiveProps(){
-    //     this.props.counterNumber
-    // }
-
-
-    reduce = () => {
-        if(this.props.counterNumber != 0){
-            this.setState((preState) => ({
-                number: preState.number - 1
-                })
-            )
-        }
-        
-        this.props.countTotal(-1)
-    }
-
-
     render() {
-        return (
+      return (
           <div>
-            <button onClick={this.increase}>+</button>
-            <mark>{this.state.number}</mark>
-            <button onClick={this.reduce}>-</button>
+              <button onClick={this.increase}>+</button>
+              <mark>{this.state.number}</mark>
+              <button onClick={this.decrease}>-</button>
           </div>
-        );
-    }
+      )
+  }
 }
 
-export default Counter;
+export default Counter
+
+
