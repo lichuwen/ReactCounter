557fda44d6d56c22d87989b7f08aa5af9314e9c3
lichuwen <545759585@qq.com>
2020-08-03 15:26:10 +0800

    add param in App to CounterGroup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <CounterGroup/>
+      <CounterGroup count={13}/>
     </div>
   );
 }
--- src/component/GroupCounter/index.jsx
+++ src/component/GroupCounter/index.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import Counter from '../Counter/index.jsx'
 
 class CounterGroup extends React.Component{
+    constructor(props){
+        super(props)
+    }
     render(){
         return(
-            new Array(3).fill(0).map((key,index) => <Counter key={index}/>)
+            new Array(this.props.count).fill(0).map((key,index) => <Counter key={index}/>)
         );
     }
 }
