df5499f3714c40924d119628dd1d6c223ec9c9d6
lichuwen <545759585@qq.com>
2020-08-04 07:31:16 +0800

    [karen] feature:add css to homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import CounterGroup from './component/GroupCounter/index.jsx'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <CounterGroup count={this.state.count}/> */}
-      <CounterGroup />
+      <div className="App-header">
+          <CounterGroup />
+      </div>
     </div>
   );
 }
